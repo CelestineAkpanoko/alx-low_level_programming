fab0b238efb910c3b5893adbbbbfee264076bc24
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-06-18 18:59:11 +0100

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,20 +10,20 @@ int main(void)
 	int i, j;
 
 	for (i = 0; i < 99; i++)
+	{
+		for (j = i + 1; j < 100; j++)
 		{
-			for (j = i + 1; j < 100; j++)
-			{
-				putchar((i / 10) + '0');
-				putchar((i % 10) + '0');
-				putchar(' ');
-				putchar((j / 10) + '0');
-				putchar((j % 10) + '0');
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
 
-				if (i == 98 && j == 99)
-					continue;
+			if (i == 98 && j == 99)
+				continue;
 
-				putchar(',');
-				putchar(' ');
+			putchar(',');
+			putchar(' ');
 			}
 		}
 	putchar('\n');
