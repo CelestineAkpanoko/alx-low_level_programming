e418b89edf8b288a7467ce52b934622d7086c55e
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-06-18 17:36:43 +0100

    different possible combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,7 +14,10 @@ int main(void)
 		for (j = 1; j < 10; j++){
 			putchar((i % 10) + '0');
 			putchar((j % 10) + '0');
-
+			if ( i == j)
+			{
+				continue;
+			}
 			putchar(',');
 			putchar(' ');
 		}
