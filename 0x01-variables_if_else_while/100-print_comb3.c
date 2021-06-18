943a057378cc976b5d3498751e307b849c26b714
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-06-18 17:42:07 +0100

    different possible combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,17 @@ int main(void)
 {
 	int i;
 	int j;
+
 	for (i = 0; i < 9; i++)
 	{
-		for (j = 1; j < 10; j++){
-			putchar((i % 10) + '0');
-			putchar((j % 10) + '0');
-			if ( i == j)
+		for (j = 1; j < 10; j++)
+		{
+			if (i == j)
 			{
 				continue;
 			}
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
 			putchar(',');
 			putchar(' ');
 		}
