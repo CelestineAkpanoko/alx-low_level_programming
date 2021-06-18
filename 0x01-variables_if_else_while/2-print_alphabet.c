b3bceaf7d08a6ad9d3ae9c25b036b3e48a3070fa
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-06-18 14:31:22 +0100

    prints alphabet in lower cases

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet in lowercase with putchar
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch < 'z'; ch++)
+	putchar("%c", ch);
+
+return (0);
+}
