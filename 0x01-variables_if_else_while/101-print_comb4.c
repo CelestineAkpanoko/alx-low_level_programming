4c1004057d883559966ff53ac4d6a0b3e1008814
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-06-18 18:10:56 +0100

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,14 +20,13 @@ int main(void)
 				putchar((x % 10) + '0');
 				putchar((y % 10) + '0');
 				putchar((z % 10) + '0');
-				
 				if (x != 7 && y != 8 && z != 9)
 				{
 					putchar(',');
 					putchar(' ');
 				}
 				else
-					continue;
+					break;
 			}
 		}
 	}
