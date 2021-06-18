6d78bac121c6d8e691765b1f34701ab6ed8e995b
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-06-18 15:00:57 +0100

    print decimal numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit number of base 10.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	for (int i = 0; i <= 10; i++)
+		printf(i);
+return (0);
+}
