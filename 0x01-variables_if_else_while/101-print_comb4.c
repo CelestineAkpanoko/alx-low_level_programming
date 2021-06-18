7b14ecbcf94208a33327e08722e44c64f7d04cbe
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-06-18 18:26:33 +0100

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,24 +13,17 @@ int main(void)
 
 	for (x = 0; x < 8; x++)
 	{
-		for (y = 1; y < 9; y++)
+		for (y = x + 1; y < 9; y++)
 		{
-			for (z = 2; z < 10; z++)
+			for (z = y + 1; z < 10; z++)
 			{
-				if (x >= y && x >= z)
-				{
-					if (y >= z)
-						continue;
-				}
 				putchar((x % 10) + '0');
 				putchar((y % 10) + '0');
 				putchar((z % 10) + '0');
+				if (x == 7 && y == 8 && z == 9)
+					continue;
 				putchar(',');
 				putchar(' ');
-				if (x == 7 && y == 8 && z == 9)
-				{
-					break;
-				}
 			}
 		}
 	}
