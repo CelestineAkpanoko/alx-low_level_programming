cbfb2b04711ad668cc1d1787c11d750ae477bcb3
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-06-24 16:25:17 +0100

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,7 +15,11 @@ void more_numbers(void)
 		int j;
 
 		for (j = 0; j <= 14; j++)
-			_putchar(j);
+		{
+			if (i > 9)
+				_putchar((i / 10) + '0');
+			_putchar((j % 10) + '0');
+		}
 		_putchar('\n');
 		i++;
 	}
