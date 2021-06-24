b067ab9e640402914deab8d3850e4bd7e2da73b5
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-06-24 16:59:49 +0100

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,15 +8,14 @@
 void print_square(int size)
 {
 	int h, w;
-	
+
 	if (size > 0)
 	{
 		for (h = 0; h < size; h++)
 		{
 			for (w = 0; w < size; w++)
 				_putchar('#');
-
-			if(h == size - 1)
+			if (h == size - 1)
 				continue;
 			_putchar('\n');
 		}
