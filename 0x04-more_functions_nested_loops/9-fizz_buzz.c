5ca28e0e4e1f3837c0646e25fdf36a3a7c3528fe
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-06-24 17:35:42 +0100

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3) == 0 && (i % 5) == 0)
+		if (((i % 3) == 0) && ((i % 5) == 0))
 			printf("FizzBuzz");
 
 		else if ((i % 3) == 0)
@@ -29,6 +29,7 @@ int main(void)
 
 		printf(" ")
 	}
+
 	printf("\n");
 
 	return (0);
