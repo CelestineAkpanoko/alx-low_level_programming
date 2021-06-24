e24b2df99a5b71778c166cd89fa70ab5932a117d
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-06-24 15:23:03 +0100

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9.
+ */
+
+void print_numbers(void)
+{
+	int i;
+	i = 48;
+
+	while (i <= 57)
+		_putchar(i);
+
+	_putchar('\n');
+}
