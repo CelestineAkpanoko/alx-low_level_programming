cb221b64818c79bf750f104086b8df023e6b7dfd
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-06-24 16:51:50 +0100

    print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,13 +11,18 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
-		for (i = 1; i <= n; i++)
+		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j < i; j++)
-			{
-				_putchar('\\');
-				_putchar('\n');
-			}
+				_putchar(' ');
+			_putchar('\\');
+			
+			if ( i == n - 1)
+				continue;
+
+			_putchar('\n');
 		}
 	}
+
+	_putchar('\n');
 }
