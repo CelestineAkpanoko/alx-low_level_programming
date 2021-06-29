ee550634560f9f7ed791ee5004ac4dd91e4bafaf
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-06-29 17:36:03 +0100

    creates and return a copy of a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - creates a copy of a string
+ * @str: the original string
+ * @dest: the copy
+ * R
+ *
+ * Return: the copy dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0, x = 0;
+
+	while (*(src + 1) != '\0')
+		l++;
+
+	for ( ; x < l ; x++)
+		dest[x] = src[x];
+
+	dest[l] = '\0';
+
+	return (dest);
+}
