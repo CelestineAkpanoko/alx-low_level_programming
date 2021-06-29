076bc3632527019777989bb99bfd676a460eb1d2
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-06-29 17:31:57 +0100

    prints element of an array of ints

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints elements of an array of integers
+ * @a: the array
+ * @n: num of elemet of the array
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if ( i < n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
