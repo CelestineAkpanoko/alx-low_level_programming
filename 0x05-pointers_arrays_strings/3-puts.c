065f3b9da737bb0edf8104884cb3f00e987ac3bd
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-06-29 16:43:55 +0100

    prints string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _puts - prints a string followed by a new line.
+ * @str: string to be printed.
+ */
+
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++)
+
+		_putchar('\n');
+}
