4722c0af7ef4e6d54f50435fe6d160e6f5b23d32
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-06-29 17:50:07 +0100

    converts string to ints

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * _atoi - convert strings to integers
+ * @s: strings
+ *
+ * Return: the int, if not then 0.
+ */
+int __atoi(char *s)
+{
+	int i, z, p;
+
+	z = 0, p = -1;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+			p *= -1;
+		if (s[i] > 47 && s[i] < 58)
+		{
+			if (z < 0)
+				z = (z * 10) - (s[i] - '0');
+			else
+				z = (s[i] - '0') * -1;
+
+			if (s[i + 1] < 48 || s[i + 1] > 57)
+				break;
+		}
+	}
+	if (p < 0)
+		z *= -1;
+	return (z);
+}
