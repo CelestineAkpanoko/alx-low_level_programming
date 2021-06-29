5a2929a7dd92ad393ccdede0118dd906f39dd629
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-06-29 16:35:15 +0100

    returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @str: the string
+ *
+ * Return: the length of str.
+ */
+
+size_t _strlen(const char *str)
+{
+	size_t length = 0;
+
+	while(*str++)
+		length++;
+
+	return (length);
+}
