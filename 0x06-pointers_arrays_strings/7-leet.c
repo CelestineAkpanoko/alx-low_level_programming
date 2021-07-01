6219a042df67f687ab9b156b1bfb7bdff8b4872e
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-07-01 16:54:25 +0100

    encode to 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,29 +1,28 @@
 #include "holberton.h"
-
 /**
- * leet - encodes to 1337
- * @str: string to be encoded
+ * leet - Encodes a string into 1337
+ * @str: The source string
  *
- * Return: encoded string.
+ * Return: The string
  */
 char *leet(char *str)
 {
-	int i = 0, n = 5, j;
-	char letter[5] = {'A', 'E', 'O', 'T', 'L'};
-	char number[5] = {'4', '3', '0', '7', '1'};
-
-	while (str[i])
-	{
-		j = 0;
-
-		while (j < n)
-		{
-			if (str[i] == letter[j] || (str[i] - 32) == number[j])
-				str[i] = number[j];
-
-			j++;
-		}
-		i++;
-	}
-	return (str);
+  int idx;
+  char mappings[26 * 2];
+  for (idx = 0; idx < 26 * 2; idx++)
+    mappings[idx] = 0;
+  mappings['a' - 'a'] = mappings['A' - 'A' + 26] = '4';
+  mappings['e' - 'a'] = mappings['E' - 'A' + 26] = '3';
+  mappings['o' - 'a'] = mappings['O' - 'A' + 26] = '0';
+  mappings['t' - 'a'] = mappings['T' - 'A' + 26] = '7';
+  mappings['l' - 'a'] = mappings['L' - 'A' + 26] = '1';
+  for (idx = 0; *(str + idx) != '\0'; idx++)
+  {
+    int is_lower = *(str + idx) >= 'a' && *(str + idx) <= 'z';
+    int is_upper = *(str + idx) >= 'A' && *(str + idx) <= 'Z';
+    int key = *(str + idx) - (is_lower * 'a') - (is_upper * ('A' - 26));
+    if ((is_lower || is_upper) && mappings[key] != 0)
+      *(str + idx) = mappings[key];
+  }
+  return (str);
 }
