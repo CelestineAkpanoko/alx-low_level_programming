92569192a89666d1abbf1c647d9a1b0094496ff0
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-07-01 02:46:53 +0100

    functions prototypes

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,5 +1,5 @@
-#ifndef "HOLBERTON.H"
-#define "HOLBERTON.H"
+#ifndef "_HOLBERTON_H_"
+#define "_HOLBERTON_H_"
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -15,3 +15,4 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
+#endif
