3a4c619199a1b6866b3bab49443d8d3bbb824f96
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-07-01 15:23:31 +0100

    rot13 encoding

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * rot13 - encode character in rot13 format
+ * @str - string to be encoded.
+ *
+ * Return: encoded string
+ */
+char *rot13(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		str[i] = str[i] + 13;
+	}
+	return (str);
+}
