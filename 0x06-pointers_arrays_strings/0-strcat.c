a1c22ebf9871ee72a9b8901d22a1862f897d2efe
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-07-01 02:42:44 +0100

    concatenate strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,18 +14,14 @@ char *_strcat(char *dest, char *src)
 	i = 0;
 
 	while (dest[i] != '\0')
+		i++;
+	j = 0;
+	while (src[j] != '\0')
 	{
-		if (dest[i] == '\0')
-		{
-			j = 0;
-			while (src[j] != '\0')
-			{
-				dest[i] = src[j];
-				j++;
-				i++;
-			}
-		}
+		dest[i] = src[j];
+		j++;
 		i++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
