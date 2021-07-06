e0234a4724349b578289d6be983ccf77ffd52a6b
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-07-06 04:54:59 +0100

    checks for character in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates a charater in a string
+ * @s: the string
+ * @c: the character to be located
+ *
+ * Return: a pointer to the first occurence of c in s, or NULL if not found.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if (c == *(s + i))
+			s[i] = *(s + i);
+		else
+			return NULL;
+	}
+	return (s);
+}
