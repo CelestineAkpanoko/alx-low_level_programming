57d1f8205a5b2daf7e352245959c3fc4bd619b7c
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-07-06 03:01:32 +0100

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,8 +11,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
 
-	for (; i < n; i++)
-		s[i] = b;
-
+	for (; n > 0; i++)
+	{
+		*(s + i) = b;
+		n--;
+	}
 	return (s);
 }
