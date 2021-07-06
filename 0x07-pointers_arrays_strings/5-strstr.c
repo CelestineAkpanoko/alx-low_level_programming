c2baf791d4f404ce9582e4093a03551940227a00
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-07-06 13:12:08 +0100

    Needle in Haystack

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * _strstr - finds the first occurence of the substring needle 
+ *	in the string haystack.
+ * @haystack: the parent string
+ * @needle: substring
+ *
+ * Return: pointer to the beginning of the located substring, or NULL.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	while (*haystack)
+	{
+		while (*needle)
+		{
+			if (*haystack == *needle)
+			{
+				*haystack++;
+				*needle++;
+			}
+			else
+				break;
+		}
+		if (*needle == '\0')
+			return (haystack);
+	}
+}
+
