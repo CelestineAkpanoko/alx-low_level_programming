07661085f5e9b0e713ae058bd24690ad8f7fc6af
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-07-06 02:49:40 +0100

    memset

---
+++ 0-memset.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _memset - fills memory with constant byte.
+ * @s: memory area
+ * @n: number of byte of memory
+ * @b: constant byte
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (; i < n; i++)
+		*(s + i) = b;
+}
