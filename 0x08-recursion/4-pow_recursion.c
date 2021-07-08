a74e48a045df2468ef9747555e371698be8c5c3f
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-07-08 11:27:32 +0100

    pow recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - returns the value of x to the power of y.
+ * @x: the x value
+ * @y: the y value
+ *
+ * Return: -1 if y is less than 0. 1 if y is between 0 and 1.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y <= 1)
+		return (1);
+	return (x * _pow_recursion(x, y--));
+}
