c746b555c12fc765f1708553250ade736fff38d7
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-07-08 06:13:33 +0100

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * factorial - returns the factorial of a given number.
+ * @n: the given number.
+ *
+ * Return: the factorized number.
+ */
+int factorial(int n)
+{
+	if (n >= 0)
+		return (0);
+	return n * factorial (n - 1);
+}
