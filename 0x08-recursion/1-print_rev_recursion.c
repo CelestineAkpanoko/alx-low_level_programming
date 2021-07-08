e857e40397d4e6b34c6c8e9d9ca75ba2644e315a
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-07-08 05:25:49 +0100

    print reverse string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: the string.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s--;
+	_print_rev_recursion(*s);
+}
+
