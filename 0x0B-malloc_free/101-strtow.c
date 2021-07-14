54c73bfa888aafa0612c95990b9741c00b5b57cd
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-07-14 19:08:19 +0100

    count words in a string

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * wrdcnt - counts the number of words in a string
+ * @s: string to be counted.
+ *
+ * Return: int of number of words.
+ */
+
+int wrdcnt(char *s)
+{
+	int i, n = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == ' ')
+		{
+			if (s[i + 1] != ' ' && s[i + 1] != '\0')
+				n++;
+		}
+		else if (i == 0)
+			n++;
+	}
+	n++;
+	return (n);
+}
