b9a45c7314c607f807900489216ed3ed8d34acce
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-07-14 19:22:31 +0100

    count words in a string

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -24,3 +24,53 @@ int wrdcnt(char *s)
 	cnt++;
 	return (cnt);
 }
+
+/**
+ * strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings
+ */
+char **strtow(char *str)
+{
+	int i, j, k, l, cnt = 0, wc = 0;
+	char **w;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	cnt = wrdcnt(str);
+	if (cnt == 1)
+		return (NULL);
+	w = (char **)malloc(cnt * sizeof(char *));
+	if (w == NULL)
+		return (NULL);
+	w[cnt - 1] = NULL;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+		{
+			for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
+				;
+			j++;
+			w[wc] = (char *)malloc(j * sizeof(char));
+			j--;
+			if (w[wc] == NULL)
+			{
+				for (k = 0; k < wc; k++)
+					free(w[k]);
+				free(w[cnt - 1];
+				free(w);
+				return (NULL);
+			}
+			for (l = 0; l < j; l++)
+				w[wc][l] = str[i + l];
+			w[wc][l] = '\0';
+			wc++;
+			i += j;
+		}
+		else
+			i++;
+	}
+	return (w);
+}
