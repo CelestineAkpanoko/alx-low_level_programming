26fa00039cb0a39a4e455a494d857dece70acc64
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-07-14 17:21:56 +0100

    string duplicate

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * _strdup - returns a pointer to a newly alloacated memory,
+ * whcih contains a copy of a string given as a parameter.
+ * @str: the string.
+ *
+ * Return: NULL if str is null. On success, pointer to the duplicated string.
+ */
+char *_strdup(char *str)
+{
+	char *dupstr;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+	dupstr = malloc(sizeof(char) * strlen(str));
+
+	i = 0;
+	while (i < '\0')
+	{
+		dupstr[i] = str[i];
+		i++;
+	}
+	dupstr[i] = '\0';
+	return (dupstr);
+}
+
