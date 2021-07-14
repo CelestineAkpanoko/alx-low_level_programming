7835d2ee6c73e155da359125e5b7e1bfda5a4a41
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-07-14 18:43:59 +0100

    allocate grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,7 +27,7 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 	}
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
 			grid[i][j] = 0;
