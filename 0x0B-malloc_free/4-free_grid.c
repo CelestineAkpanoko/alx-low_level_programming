976f76e29c087716a8d6d9e145e3a77c8782c462
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-07-14 18:47:31 +0100

    free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * free_grid -  frees a 2D grid previously created.
+ * @grid: the grid
+ * @height: the height of the grid.
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	 for (i = 0; i < height; i++)
+		 free(grid[i]);
+	 free(grid);
+}
+
