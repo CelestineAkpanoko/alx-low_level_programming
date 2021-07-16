3183da83c70decd9bb6511146406d064809ad8ad
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-07-16 13:02:51 +0100

    header file updated

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,6 +1,8 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <stdlib.h>
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
