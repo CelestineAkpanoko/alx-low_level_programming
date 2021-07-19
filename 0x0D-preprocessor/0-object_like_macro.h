66272748dfb67d32515c1912c3635a2f777a314c
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-07-19 21:39:38 +0100

    define a size macro with 1024 token

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,7 +1,7 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 /**
- * defined a macro SIZE with token 1024
+ * SIZE - a macro with token 1024
  */
-
-#ifndef SIZE
 #define SIZE 1024
 #endif
