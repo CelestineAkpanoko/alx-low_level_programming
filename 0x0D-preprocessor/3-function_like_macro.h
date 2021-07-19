82fcca64a9f43708501bc4e4273b13f2aef33372
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-07-19 21:54:42 +0100

    returns an absolute value of a number

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * ABS - computes the absolute value of a number.
+ * @x: the number
+ */
+#define ABS(x) (x < 0 ? x * -1 : x)
+#endif
