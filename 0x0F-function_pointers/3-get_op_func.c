2a38ddbbf063827c08d4918b1b00c936d396d177
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-07-21 15:52:46 +0100

    task 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "calc.h"
+#include <stdlib.h>
+
+/**
+ * get_op_func - gets the operator to be used
+ * @s: the operator string passed.
+ *
+ * Return: a pointer to the function that corresponds to the operator.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	if (s != '+' || s != '-' || s != '*' || s != '/' || s != '%')
+		return (NULL);
+
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i = 0;
+	
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
+
+	return (ops[i].f);
+}
+
+	
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sums two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the sum of a and b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substracts two numbers
+ * @a: the first number
+ * @b: second number
+ *
+ * Return: result of the subtraction.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b );
+}
+
+/**
+ * op_div - divides two numbers
+ * @a:  first number
+ * @b: second number
+ *
+ * Return: the dividion of two numbers
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the multiplication of 2 numbers
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_mod - obtains the remainder from a divided number
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
