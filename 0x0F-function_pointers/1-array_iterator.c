2110cb155981a388c2905664361125897e5b7ba3
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-07-21 14:15:23 +0100

    executes function given as a parameter on each element of an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,12 +9,12 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array == NULL || array == NULL)
+	if (array == NULL || action == NULL)
 		return;
 
 	while (size-- > 0)
 	{
-		action(*array);
+		(*action)(*array);
 		array++;
 	}
 }
