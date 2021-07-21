f08a12bb2b612adea44ac069e1dde1d7ce57d81d
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-07-21 14:59:12 +0100

    searches for the index of an element in an array

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -21,10 +21,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	for (i = 0; i < size; i++)
 	{
-		if (array[i] == cmp(array[i]))
+		if (cmp(array[i]) != 0)
 			return (i);
-		else
-			return (-1);
 	}
+	return (-1);
 }
 
