066973d278c4de22d2ae1ec32ea0ef4592d6cbae
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-07-22 22:20:44 +0100

    print numbers separated by a separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,11 +19,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		return;
 
 	for (i = 0; i < n; i++)
-		printf("%i%s ", va_arg(ap, unsigned int), separator);
-
+	{
+		printf("%i", va_arg(ap, unsigned int));
+		if (i != n - 1)
+			printf("%s ", separator);
+		else
+			printf("\n");
+	}
 	va_end(ap);
-
-	printf("\n");
-
 }
 
