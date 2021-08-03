7a84e78e383683cac208d7cb599dd8ff83818cb7
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-08-03 19:23:41 +0100

    adds a new node

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a node to the linked list.
+ * @head: pointer to the head of the linked list
+ * @n: value of new node
+ *
+ * Return: struct
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	if (head == NULL)
+	{
+		(*head)->next = new_node;
+	}
+	else
+	{
+		new_node->next = *head;
+		(*head) = new_node;
+	}
+	return (new_node);
+}
