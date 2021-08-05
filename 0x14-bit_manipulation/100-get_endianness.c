0e1d0f11c1180a79aaa903990362a291c4be3ca2
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-08-05 17:00:50 +0100

    task-6

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * get_endianness - Checks the endianness.
+ *
+ * Return: If big-endian - 0.
+ *         If little-endian - 1.
+ */
 int get_endianness(void)
 {
 	int num = 1;
